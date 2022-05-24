c38a8a7de717e29d2b03b404ac397beaae367449
Mario <mario-tomic@hotmail.com>
2022-05-24 13:50:52 +0200

    slight shotgun pellets fix

--- src/Entities/Weapons/Shotgun.java
+++ src/Entities/Weapons/Shotgun.java
@@ -3,7 +3,7 @@ package Entities.Weapons;
 import Entities.Entity;
 
 public class Shotgun extends Weapon{
-    private static final int PELLETS = 6;
+    private static final int PELLETS = 7;
     private static final double PELLET_SPREAD = Math.PI / 12;
     public Shotgun(Entity entity) {
         super(entity, 30, 3, 10);
@@ -12,9 +12,12 @@ public class Shotgun extends Weapon{
     @Override
     protected Projectile[] generateProjectiles(double angle) {
         Projectile[] projectiles = new Projectile[PELLETS];
-        for(int i = 0, j = PELLETS - 1; i <= j; ++i, --j){
-            projectiles[i] = new Projectile(e.getX(), e.getY(), angle - i * PELLET_SPREAD , PROJECTILE_SPEED, PROJECTILE_SIZE);
+        for(int i = 0, j = PELLETS - 1; i < j; ++i, --j){
+            projectiles[i] = new Projectile(e.getX(), e.getY(), angle - (i+1) * PELLET_SPREAD , PROJECTILE_SPEED, PROJECTILE_SIZE);
             projectiles[j] = new Projectile(e.getX(), e.getY(), angle + (i+1) * PELLET_SPREAD  , PROJECTILE_SPEED, PROJECTILE_SIZE);
+        }
+        if(PELLETS % 2 == 1){
+            projectiles[PELLETS / 2] = new Projectile(e.getX(), e.getY(), angle , PROJECTILE_SPEED, PROJECTILE_SIZE);
         }
         return projectiles;
     }
