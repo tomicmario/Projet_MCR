67f2e40f84970b8a999f786dbcbb476fe0784896
yanik23 <ylange@windowslive.com>
2022-06-09 22:39:19 +0200

    commenting behaviour
    Signed-off-by: yanik23 <ylange@windowslive.com>

--- src/GameObjects/Entities/Enemy/Behaviour/Behaviour.java
+++ src/GameObjects/Entities/Enemy/Behaviour/Behaviour.java
@@ -3,35 +3,72 @@ package GameObjects.Entities.Enemy.Behaviour;
 import GameObjects.Entities.Enemy.Enemy;
 import GameObjects.Entities.Entity;
 
-
+/**
+ * Abstract class used to define all the behaviours of the IA trough inheritance.
+ */
 public abstract class Behaviour {
     protected final Enemy e;
     protected final Entity target;
 
+    /**
+     * Behaviour Constructor. Used to initialize common values of all
+     * classes that inherits of Behaviour (in this case all the behaviours of the game).
+     *
+     * @param e : The enemy on which we affect the behaviour.
+     * @param target : The player on which the enemy will behave depending on the behaviour.
+     */
     protected Behaviour(Enemy e, Entity target){
         this.e = e;
         this.target = target;
     }
 
-    public abstract void move();
-
+    /**
+     *
+     * @return The angle needed by the enemy used to move toward the target/player. // TODO.
+     */
     protected double getAngle(){
         return Math.atan2(target.getY() - e.getY(), target.getX() - e.getX()) - Math.PI / 4;
     }
 
+    /**
+     *
+     * @return The distance between the enemy and the target/player.
+     */
     protected int getDistance(){
         return (int)Math.round( Math.sqrt( (target.getX() - e.getX()) * (target.getX() - e.getX()) + (target.getY() - e.getY()) * (target.getY() - e.getY()) ) );
     }
 
+    /**
+     *
+     * @param angle : The angle of the enemy used to calculate the speed on the axis x.
+     * @return A delta x that can be added to the current x to change the position (equivalent as speed).
+     */
     protected int getSpeedX(double angle){
         return (int) (e.getSpeed() * Math.cos(angle) - e.getSpeed() * Math.sin(angle));
     }
 
+    /**
+     *
+     * @param angle : The angle of the enemy used to calculate the speed on the axis y.
+     * @return A delta y that can be added to the current y to change the position (equivalent as speed).
+     */
     protected int getSpeedY(double angle){
         return (int) (e.getSpeed() * Math.sin(angle) + e.getSpeed() * Math.cos(angle));
     }
 
+    /**
+     * Moves an enemy by setting new coordinates.
+     *
+     * @param speedX : The speed x or delta x that will be added to the current x to move the enemy.
+     * @param speedY : The speed y or delta y that will be added to the current y to move the enemy.
+     */
     protected void moveEntity(int speedX, int speedY){
         e.setCoordinates(e.getX() + speedX, e.getY() + speedY);
     }
+
+    /**
+     * Abstract move function used in all the classes that inherits of Behaviour.
+     * Moves the enemy depending on the behaviour.
+     */
+    public abstract void move();
 }
