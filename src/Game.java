7ea98ebda5b4744a30fb681563eb67c7d0b795f0
Mario Tomic <mario-tomic@hotmail.com>
2022-06-19 16:13:06 +0200

    all comments done

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,12 @@
 import Controller.Controller;
 
+/**
+ * Entry point of the application, launches the controller
+ *
+ * @author Janis Chiffelle, Yanik Lange, Mario Tomic
+ * @date 19.06.2022
+ * @version Java 11
+ */
 public class Game {
     public static void main(String ... args) {
         new Controller().run();
